5ce31acf61970ea37f80b591d843213f66e5b6cb
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-08-17 18:09:42 +0530

    feat: wire BrowserRouter into application entry

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import './App.css'
 import './mobile-prototype.css'
+import './route-prototype.css'
+import { AppRouter } from './app/AppRouter.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   </StrictMode>,
 )
