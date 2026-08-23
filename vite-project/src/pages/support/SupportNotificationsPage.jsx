a45932cc28f64db08d04f2c507b0ccb506250b09
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-08-23 22:29:55 +0530

    Create dedicated support notifications page

---
+++ vite-project/src/pages/support/SupportNotificationsPage.jsx
@@ -0,0 +1,7 @@
+import { SupportPageV3 } from './SupportPageV3'
+
+export function SupportNotificationsPage() {
+  return <SupportPageV3 section="notifications" />
+}
+
+export default SupportNotificationsPage
